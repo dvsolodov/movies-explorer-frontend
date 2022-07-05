e076bd547e1846874ab1193e342a35ac0356e963
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-05 18:24:26 +1000

    feat: create routintg

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,21 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Switch>
+        <Route path="/movies">
+        </Route>
+
+        <Route path="/saved-movies">
+        </Route>
+
+        <Route path="/profile">
+        </Route>
+
+        <Route path="/signin">
+        </Route>
+
+        <Route path="/signup">
+        </Route>
+
+        <Route path="/signout">
+          <Redirect to="/signin" />
+        </Route>
+
+        <Route>
+          { loggedIn ? <Redirect to="/" /> : <Redirect to="/signin" /> }
+        </Route>
+
+      </Switch>
     </div>
   );
 }
