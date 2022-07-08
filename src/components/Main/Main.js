fcfdf6ede357bc0d56c4796c8753905ae11e3579
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-08 21:54:08 +1000

    feat: add Header component to Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,17 +1,9 @@
-import { Link } from 'react-router-dom';
+import Header from '../Header/Header';
 
 function Main() {
   return (
     <main className="content">
-      <h1>О проекте</h1>
-      <nav>
-        <Link to="/">О проекте</Link>
-        <Link to="/movies">Фильмы</Link>
-        <Link to="/saved-movies">Сохраненные фильмы</Link>
-        <Link to="/profile">Профиль</Link>
-        <Link to="/signin">Вход</Link>
-        <Link to="/signup">Регистрация</Link>
-      </nav>
+      <Header />
     </main>
   );
 }
