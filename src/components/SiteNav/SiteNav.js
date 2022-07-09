33be48095e53c3db10bde561cfc750b63f9bd007
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-09 11:07:09 +1000

    feat: add markup and styles for SiteNav component

---
+++ src/components/SiteNav/SiteNav.js
@@ -0,0 +1,19 @@
+import './SiteNav.css';
+import { Link } from 'react-router-dom';
+
+export default function SiteNav() {
+  return (
+    <ul className="site-nav">
+      <li className="site-nav__item">
+        <Link className="site-nav__link site-nav__link_active" to="/movies">
+          Фильмы
+        </Link>
+      </li>
+      <li className="site-nav__item">
+        <Link className="site-nav__link" to="/saved-movies">
+          Сохранённые фильмы
+        </Link>
+      </li>
+    </ul>
+  );
+}
