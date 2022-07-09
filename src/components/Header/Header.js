9d547ddbdbb9894b3f3198990ed4a5adf5dbf5f5
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-09 11:01:57 +1000

    feat: add markup and styles from Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+import Logo from '../Logo/Logo';
+import Navigation from '../Navigation/Navigation';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <Logo />
+      <Navigation />
+    </header>
+  );
+}
