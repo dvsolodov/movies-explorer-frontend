a7c90a4862eff6bd00e996b0ef7dc58e93db3608
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-09 10:59:27 +1000

    feat: include styles and Promo component in Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,12 @@
+import './Main.css';
 import Header from '../Header/Header';
+import Promo from '../Promo/Promo';
 
 function Main() {
   return (
     <main className="content">
       <Header />
+      <Promo />
     </main>
   );
 }
