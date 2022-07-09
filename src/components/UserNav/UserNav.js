c1c7d4eec4b7d55cf2ce18951610155571c0d938
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-09 11:07:55 +1000

    feat: add markup and stayles for UserNav component

---
+++ src/components/UserNav/UserNav.js
@@ -0,0 +1,24 @@
+import './UserNav.css';
+import { Link } from 'react-router-dom';
+
+export default function UserNav() {
+  return (
+    <ul className="user-nav">
+      <li className="user-nav__item">
+        <Link className="user-nav__link" to="/register">
+          Регистрация
+        </Link>
+      </li>
+      <li className="user-nav__item">
+        <Link className="user-nav__link user-nav__link_enter" to="/login">
+          Войти
+        </Link>
+      </li>
+      <li className="user-nav__item">
+        <Link className="user-nav__link" to="/profile">
+          Аккаунт
+        </Link>
+      </li>
+    </ul>
+  );
+}
