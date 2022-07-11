a6f8919ecb83d0664ed24c64ea5055c5d6c00dc5
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-11 23:21:05 +1000

    feat: add AboutMe and Techs in Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,16 @@
 import './Main.css';
-import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
 
-function Main() {
+export default function Main() {
   return (
-    <main className="content">
-      <Header />
+    <main className="main">
       <Promo />
       <AboutProject />
+      <Techs />
+      <AboutMe />
     </main>
   );
 }
-
-export default Main;
