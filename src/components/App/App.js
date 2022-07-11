1e544a6d5cc9114030a4543dfdc3c9586ca9ed5a
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-11 23:19:39 +1000

    feat: add Header and Footer in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,28 +7,25 @@ import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
 import NotFound from '../NotFound/NotFound';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="app">
+      <Header />
+
       <Routes>
-
         <Route path="/" element={<Main />} />
-
         <Route path="/movies" element={<Movies />} />
-
         <Route path="/saved-movies" element={<SavedMovies />} />
-
         <Route path="/profile" element={<Profile />} />
-
         <Route path="/signin" element={<Login />} />
-
         <Route path="/signup" element={<Register />} />
-
         <Route path="*" element={<NotFound />} />
-
       </Routes>
 
+      <Footer />
     </div>
   );
 }
