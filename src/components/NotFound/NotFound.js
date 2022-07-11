a5801b0ad7a2732d44e9e4adf73ec644bcbd1fb7
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-11 23:25:21 +1000

    feat: create NotFound component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,23 @@
+import './NotFound.css';
+import { useNavigate} from 'react-router-dom';
+
 function NotFound() {
+  const navigation = useNavigate();
+  const goBack = () => {
+    navigation(-1);
+  }
+
   return (
-    <section className="movies">
-      <h1>Страница не найдена</h1>
+    <section className="not-found">
+      <div className="not-found__wrap">
+        <h1 className="not-found__title">404</h1>
+        <p className="not-found__under-title">
+          Страница не найдена
+        </p>
+        <button className="not-found__btn" onClick={goBack}>
+          Назад
+        </button>
+      </div>
     </section>
   );
 }
