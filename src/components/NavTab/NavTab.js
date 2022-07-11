740cf47f1d92c513bbb5d3814a77288031804c87
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-11 23:23:45 +1000

    feat: add react-scroll in NavTab

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,22 +1,31 @@
 import './NavTab.css';
-import { Link } from 'react-router-dom';
+import { Link } from "react-scroll";
 
 export default function NavTab() {
   return (
     <nav className="nav-tab">
       <ul className="nav-tab__items">
         <li className="nav-tab__item">
-          <Link className="nav-tab__link" to="/#">
+          <Link className="nav-tab__link"
+            to="about-project"
+            smooth={true}
+          >
             О проекте
           </Link>
         </li>
         <li className="nav-tab__item nav-tab__item_center">
-          <Link className="nav-tab__link" to="/#">
+          <Link className="nav-tab__link"
+            to="techs"
+            smooth={true}
+          >
             Технологии
           </Link>
         </li>
         <li className="nav-tab__item">
-          <Link className="nav-tab__link" to="/#">
+          <Link className="nav-tab__link"
+            to="about-me"
+            smooth={true}
+          >
             Студент
           </Link>
         </li>
