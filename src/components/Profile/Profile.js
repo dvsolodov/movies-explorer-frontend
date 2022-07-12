f2698f50018b1c294369cea34399d84c3149189e
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-12 23:43:12 +1000

    feat: update Profile comp

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,35 @@
-function Profile() {
+import './Profile.css';
+import { useState } from 'react';
+
+export default function Profile() {
+  const [userName, setUserName] = useState('Денис');
+  const [userEmail, setUserEmail] = useState('example@yandex.ru');
+
+  function handleNameInputChange(e) {
+    setUserName(e.target.value);
+  }
+
+  function handleEmailInputChange(e) {
+    setUserEmail(e.target.value);
+  }
+
   return (
-    <section className="movies">
-      <h1>Профиль</h1>
+    <section className="profile">
+      <form className="profile__form">
+        <h2 className="profile__hello">Привет, Денис!</h2>
+        <label className="profile__field">
+          <p className="profile__field-title">Имя</p>
+          <input className="profile__field-data" value={userName} onChange={handleNameInputChange} />
+        </label>
+        <label className="profile__field">
+          <p className="profile__field-title">Email</p>
+          <input className="profile__field-data" value={userEmail} onChange={handleEmailInputChange} />
+        </label>
+      </form>
+      <div className="profile__actions">
+        <button className="profile__action">Редактировать</button>
+        <button className="profile__action profile__action_color_red">Выйти из аккаунта</button>
+      </div>
     </section>
   );
 }
-
-export default Profile;
