d22f7ab5feed8ba1e708c88479c70ba035521738
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-12 23:39:50 +1000

    feat: update Login comp

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,34 @@
-function Login() {
+import './Login.css';
+import { Link } from 'react-router-dom';
+import Logo from '../Logo/Logo';
+import Input from '../Input/Input';
+import AuthFormButton from '../AuthFormButton/AuthFormButton';
+
+export default function Login() {
   return (
-    <section className="movies">
-      <h1>Авторизация</h1>
+    <section className="login">
+      <Logo />
+      <h1 className="login__hello">Рады видеть!</h1>
+      <form className="login__form">
+        <Input
+           inputType="email"
+           inputTitle="E-mail"
+           inputValue="example@yandex.ru"
+           inputName="email"
+           inputError=""
+        />
+        <Input
+           inputType="password"
+           inputTitle="Пароль"
+           inputValue="09u09u0"
+           inputName="password"
+           inputError="Что-то пошло не так..."
+        />
+        <AuthFormButton buttonTitle="Войти"/>
+      </form>
+      <p className="login__is-not-account">
+        Ещё не зарегистрированы? <Link className="login__is-not-account login__is-not-account_text-enter" to="/signup">Регистрация</Link>
+      </p>
     </section>
   );
 }
-
-export default Login;
