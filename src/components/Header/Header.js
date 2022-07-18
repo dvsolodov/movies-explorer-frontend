3f89d64fd70f0a362ff08ec4cf13d50f2446f235
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-18 17:13:04 +1000

    style: adapt Header comp

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,11 @@ import './Header.css';
 import Logo from '../Logo/Logo';
 import Navigation from '../Navigation/Navigation';
 
-export default function Header() {
+export default function Header({ onNavPopup }) {
   return (
     <header className="header">
       <Logo />
-      <Navigation />
+      <Navigation onNavPopup={onNavPopup} />
     </header>
   );
 }
