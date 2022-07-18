d5ebff2377614fa3295f5b1bdbecb51af8f18625
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-18 17:13:53 +1000

    style: adapt Navigation comp

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,31 @@
 import './Navigation.css';
+import hamburger from '../../images/navigation__hamburger.svg'
 import SiteNav from '../SiteNav/SiteNav';
 import UserNav from '../UserNav/UserNav';
 
-export default function Navigation() {
+export default function Navigation({ onNavPopup }) {
+  const user = true;
+
   return (
     <nav className="navigation">
-      <SiteNav />
-      <UserNav />
+      { user &&
+        <div className="navigation__wrap_site-nav">
+          <SiteNav />
+        </div>
+      }
+
+      <div className="navigation__wrap_user-nav">
+        <UserNav isUser={user} />
+      </div>
+
+      { user &&
+        <button className="navigation__hamburger-btn" onClick={onNavPopup}>
+          <img className="navigation__hamburger-img"
+            src={ hamburger }
+            alt="Кнопка открытия панели навигации"
+          ></img>
+        </button>
+      }
     </nav>
   );
 }
