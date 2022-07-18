70ef25dbaf53d090a91833f3b304a7e764f1ea52
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-18 19:22:06 +1000

    fix: change class name in Login comp

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
-    <Link className="logo__link" to="/">
+    <Link className="logo" to="/">
       <img className="logo__img" src={logo} alt="Логотип"></img>
     </Link>
   );
