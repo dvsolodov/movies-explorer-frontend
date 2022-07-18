170c7eb0ba360f0cb13a7d1757c174555fe6d762
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-18 17:17:26 +1000

    style: adapt UserNav comp

--- src/components/UserNav/UserNav.js
+++ src/components/UserNav/UserNav.js
@@ -1,25 +1,31 @@
 import './UserNav.css';
 import { Link } from 'react-router-dom';
 
-export default function UserNav() {
+export default function UserNav({ isUser }) {
 
   return (
     <ul className="user-nav">
-      <li className="user-nav__item">
-        <Link className="user-nav__link" to="/signup">
-          Регистрация
-        </Link>
-      </li>
-      <li className="user-nav__item">
-        <Link className="user-nav__link user-nav__link_enter" to="/signin">
-          Войти
-        </Link>
-      </li>
-      <li className="user-nav__item user-nav__item_account">
-        <Link className="user-nav__link user-nav__link_account" to="/profile">
-          Аккаунт
-        </Link>
-      </li>
+      { !isUser &&
+        <li className="user-nav__item">
+          <Link className="user-nav__link" to="/signup">
+            Регистрация
+          </Link>
+        </li>
+      }
+      { !isUser &&
+        <li className="user-nav__item">
+          <Link className="user-nav__link user-nav__link_enter" to="/signin">
+            Войти
+          </Link>
+        </li>
+      }
+      { isUser &&
+        <li className="user-nav__item user-nav__item_account">
+          <Link className="user-nav__link user-nav__link_account" to="/profile">
+            Аккаунт
+          </Link>
+        </li>
+      }
     </ul>
   );
 }
