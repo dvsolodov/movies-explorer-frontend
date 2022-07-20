79b69f658cd5f202e1df62d35581617121548610
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-20 18:28:49 +1000

    hotfix: update UserNav comp

--- src/components/UserNav/UserNav.js
+++ src/components/UserNav/UserNav.js
@@ -1,28 +1,27 @@
 import './UserNav.css';
-import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 export default function UserNav({ isUser }) {
-  const location = useLocation();
-  const locPath = location.pathname;
 
   return (
     <ul className="user-nav">
-      { locPath === '/' &&
+      { !isUser &&
         <li className="user-nav__item">
           <Link className="user-nav__link" to="/signup">
             Регистрация
           </Link>
         </li>
       }
-      { locPath === '/' &&
+
+      { !isUser &&
         <li className="user-nav__item">
           <Link className="user-nav__link user-nav__link_enter" to="/signin">
             Войти
           </Link>
         </li>
       }
-      { locPath !== '/' &&
+
+      { isUser &&
         <li className="user-nav__item user-nav__item_account">
           <Link className="user-nav__link user-nav__link_account" to="/profile">
             Аккаунт
