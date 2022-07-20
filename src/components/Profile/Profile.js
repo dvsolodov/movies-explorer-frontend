b27db0bd76abc0ca418a32647c21955d5544ad35
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-20 18:23:10 +1000

    hotfix: add Header and Footer comps to Profile comp

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,8 @@
 import './Profile.css';
 import { useState } from 'react';
+import Header from '../Header/Header';
 
-export default function Profile() {
+export default function Profile({ onNavPopup }) {
   const [userName, setUserName] = useState('Денис');
   const [userEmail, setUserEmail] = useState('example@yandex.ru');
 
@@ -14,22 +15,25 @@ export default function Profile() {
   }
 
   return (
-    <section className="profile">
-      <form className="profile__form">
-        <h2 className="profile__hello">Привет, Виталий!</h2>
-        <label className="profile__field">
-          <p className="profile__field-title">Имя</p>
-          <input className="profile__field-data" value={userName} onChange={handleNameInputChange} />
-        </label>
-        <label className="profile__field">
-          <p className="profile__field-title">Email</p>
-          <input className="profile__field-data" value={userEmail} onChange={handleEmailInputChange} />
-        </label>
-      </form>
-      <div className="profile__actions">
-        <button className="profile__action">Редактировать</button>
-        <button className="profile__action profile__action_color_red">Выйти из аккаунта</button>
-      </div>
-    </section>
+    <>
+      <Header onNavPopup={onNavPopup} />
+      <main className="profile">
+        <form className="profile__form">
+          <h2 className="profile__hello">Привет, Виталий!</h2>
+          <label className="profile__field">
+            <p className="profile__field-title">Имя</p>
+            <input className="profile__field-data" value={userName} onChange={handleNameInputChange} />
+          </label>
+          <label className="profile__field">
+            <p className="profile__field-title">Email</p>
+            <input className="profile__field-data" value={userEmail} onChange={handleEmailInputChange} />
+          </label>
+        </form>
+        <div className="profile__actions">
+          <button className="profile__action">Редактировать</button>
+          <button className="profile__action profile__action_color_red">Выйти из аккаунта</button>
+        </div>
+      </main>
+    </>
   );
 }
