230ab71d8ac5c10fa882cce02e5b6f472fff555e
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-20 18:21:10 +1000

    hotfix: add Header and Footer comps to Movies comp

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,13 +2,19 @@ import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
-export default function Movies() {
+export default function Movies({ onNavPopup }) {
   return (
-    <main className="movies">
-      <SearchForm />
-      <MoviesCardList />
-      <Preloader/>
-    </main>
+    <>
+      <Header onNavPopup={onNavPopup} />
+      <main className="movies">
+        <SearchForm />
+        <MoviesCardList />
+        <Preloader/>
+      </main>
+      <Footer />
+    </>
   );
 }
