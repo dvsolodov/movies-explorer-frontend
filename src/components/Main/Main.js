29dd9a2df8f4121f0bfb3093b16387819a1d849e
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-20 18:20:32 +1000

    hotfix: add Header and Footer comps to Main comp

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,14 +3,20 @@ import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
-export default function Main() {
+export default function Main({ onNavPopup }) {
   return (
-    <main className="main">
-      <Promo />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-    </main>
+    <>
+      <Header onNavPopup={onNavPopup} />
+      <main className="main">
+        <Promo />
+        <AboutProject />
+        <Techs />
+        <AboutMe />
+      </main>
+      <Footer />
+    </>
   );
 }
