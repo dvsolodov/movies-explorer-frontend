288f4d33fa11894b263cff8ca6286fdd710a4822
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-24 00:14:06 +1000

    feat: get form data and movies in Movies comp

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,15 +4,50 @@ import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { moviesApi } from '../../utils/MoviesApi';
+import { useEffect, useState } from 'react';
 
 export default function Movies({ onNavPopup }) {
+  const [formData, setFormData] = useState({});
+  const [movies, setMovies] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  // render the page if there is data in localStorage
+  /*
+  useEffect(() => {
+    console.log(`keyWords ${keyWords}`);
+    console.log(`isShortFilm ${isShortFilm}`);
+    console.log(`movies ${movies}`);
+  }, []);
+  */
+
+  useEffect(() => {
+    setIsLoaded(false);
+    getAllMovies();
+    console.log(formData);
+    console.log(movies);
+  }, [formData]);
+
+  function handleFormData({ keyWords, isShortedFilm }) {
+    setFormData({ keyWords, isShortedFilm });
+  }
+
+  function getAllMovies() {
+    moviesApi.getMovies()
+      .then((allMovies) => {
+        setMovies(allMovies);
+        setIsLoaded(true);
+      })
+      .catch(err => console.log(err));
+  }
+
   return (
     <>
       <Header onNavPopup={onNavPopup} />
       <main className="movies">
-        <SearchForm />
-        <MoviesCardList />
-        <Preloader/>
+        <SearchForm getFormData={handleFormData} />
+        { isLoaded && <MoviesCardList movies={movies} /> }
+        { !isLoaded && <Preloader/> }
       </main>
       <Footer />
     </>
