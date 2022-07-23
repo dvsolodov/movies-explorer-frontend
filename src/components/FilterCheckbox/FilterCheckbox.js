d6c3c4919926b024044d6e6765f94347fb81b4ab
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-24 00:15:34 +1000

    feat: add name attribute to an input in the FilterCheckbox comp

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -4,7 +4,7 @@ export default function FilterCheckbox() {
   return (
     <label className="filter-checkbox">
       <div className="filter-checkbox__vertical-element"></div>
-      <input className="filter-checkbox__input" type="checkbox"></input>
+      <input className="filter-checkbox__input" type="checkbox" name="shortFilm"></input>
       <div className="filter-checkbox__text">Короткометражки</div>
     </label>
   );
