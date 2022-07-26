a7cdb859ba9fb15389a031e3521cb348a144ab2f
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-26 14:03:03 +1000

    feat: make the ability to add attribute disabled

--- src/components/AuthFormButton/AuthFormButton.js
+++ src/components/AuthFormButton/AuthFormButton.js
@@ -1,7 +1,7 @@
 import './AuthFormButton.css';
 
-export default function AuthFormButton ({buttonTitle}) {
+export default function AuthFormButton ({buttonTitle, isDisabled }) {
   return (
-    <button className="auth-form-button" type="submit">{buttonTitle}</button>
+    <button className="auth-form-button" type="submit" disabled={isDisabled}>{buttonTitle}</button>
   );
 }
