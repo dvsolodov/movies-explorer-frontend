45ff2c213bb3ccd481e553b0289797c465ac435c
Solodov Denis <d.v.solodov@yandex.ru>
2022-07-29 13:53:33 +1000

    fix: remove unused elements from ProtectedRoute comp

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,9 +1,8 @@
-import { useNavigate, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { LoggedInContext } from '../../contexts/LoggedInContext';
 
 export default function ProtectedRoute({ children }) {
-  const navigate = useNavigate();
   const loggedIn = useContext(LoggedInContext);
 
   if (!loggedIn) {
